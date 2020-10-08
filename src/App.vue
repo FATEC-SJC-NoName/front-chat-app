819bffe8ca96955120b87fb8739b4270e13f2b5c
JOE BLACK <joeprimeiro@hotmail.com>
2020-10-08 20:12:32 -0300

    atualização do front

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="columns">
+
+      <div class="column" v-if="ExibirChat">
+        <ChatBox></ChatBox>
+      </div>
+      <div class="column" v-if="ExibirDashboard">
+        <Dashboard></Dashboard>
+      </div>
+
+
+
+      <div class="column">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld  msg="Chat NoName"/>    
+      </div>
+  
+      <fab :position="position" position-type="absolute" :bg-color="bgColor" :actions="fabActions" main-icon="account_circle" 
+      @dashboard="dashboard" @chatBox="chatBox" start-opened="false" toggle-when-away="true"></fab>
+
+    </div>
+    
+
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import fab from 'vue-fab'
+import ChatBox from './components/ChatBox.vue'
+import Dashboard from './components/Dashboard.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,fab,ChatBox,Dashboard
+  },
+   data(){
+      return {
+          ExibirChat:false,
+          ExibirDashboard:false,          
+          bgColor: '#009900',
+          position: 'bottom-left',
+        
+          
+          fabActions: [
+              {
+                  icon:'dashboard',
+                  name:'dashboard'
+              },
+             {             
+                icon: 'chat',
+                name: 'chatBox',                
+              }
+          ]
+      }
+  },
+  created(){
+      this.Exibir=false
+
+  },
+  methods:{
+      dashboard(){
+         // alert('dashboard clicado');
+          this.ExibirDashboard=!this.ExibirDashboard
+      },
+      chatBox(){
+         // alert('chat clicado');
+          this.ExibirChat=!this.ExibirChat
+      }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import { faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+    faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+    faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+
+// ...
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
