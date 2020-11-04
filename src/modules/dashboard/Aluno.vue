4420d60650d7dba62fd7f55d32858069c3a063ac
Cauan Almeida <49292910+Tsundek@users.noreply.github.com>
2020-11-04 16:33:31 -0300

    Front sprint 4 (#22)
    * adicionando pacotes necessários
    
    * configuração dos pacotes
    
    * adicionando router
    
    * front end com chat
    
    * tirando codigos não utilizados
    
    * fixed issues
    
    * removing code smells

---
+++ src/modules/dashboard/Aluno.vue
@@ -0,0 +1,50 @@
+<template style="background-color:black">
+  <v-container>
+  <div class="aluno">
+    <h2 class="ma-5 font-weight-bold ">Alunos</h2>
+    <v-container class="my-5">
+
+      <v-layout row wrap>
+        <v-flex xs12 sm3 v-for="id in team" :key="id.name">
+
+          <v-card flat class="text-center ma-3" shaped>
+            <v-icon class="pt-4">mdi-account</v-icon>
+
+
+          <v-card-text>
+            <div class="subheading">{{id.name}}</div>
+            <div class="grey--text">Aluno</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn class="white" outlined>
+              <v-icon left>mdi-android-messages</v-icon>
+            </v-btn>
+          </v-card-actions>
+          </v-card>
+        </v-flex>
+
+
+
+      </v-layout>
+    </v-container>
+  </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  team: [
+        { name: 'Clara Julia'},
+        { name: 'Bruno Davi'},
+        { name: 'Aurora Isabela'},
+        { name: 'Eduarda Ayla'},
+        { name: 'Enrico André'},
+        { name: 'Alameda Waldemar'},
+        { name: 'Mirella Ester'},
+        { name: 'Kauê Carlos'},
+      ]
+})
+}
+</script>
---
+++ src/modules/dashboard/Instrutor.vue
@@ -0,0 +1,47 @@
+<template style="background-color:black">
+<v-container>
+  <div class="instrutor">
+    <h2 class="ma-5 font-weight-bold ">Instrutores</h2>
+    <v-container class="my-5">
+
+      <v-layout row wrap>
+        <v-flex xs12 sm4 v-for="id in team" :key="id.name">
+
+          <v-card flat class="text-center ma-3" shaped>
+            <v-icon class="pt-4">mdi-account</v-icon>
+
+
+          <v-card-text>
+            <div class="subheading">{{id.name}}</div>
+            <div class="grey--text">Instrutor</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn class="white" outlined>
+              <v-icon left>mdi-android-messages</v-icon>
+            </v-btn>
+          </v-card-actions>
+          </v-card>
+        </v-flex>
+
+
+
+      </v-layout>
+    </v-container>
+  </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  team: [
+        { name: 'Lerena Adriana'},
+        { name: 'Anderson da Silva'},
+        { name: 'Emily Godo'},
+        { name: 'Luiza Giovana'},
+        { name: 'Carolina Isabel'},
+      ]
+})
+}
+</script>
